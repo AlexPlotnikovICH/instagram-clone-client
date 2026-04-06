fa1f38c9b1c06d62339a599cac89662def13b9aa
AlexPlot <plotnikov1988@gmail.com>
2026-04-06 20:49:18 +0200

    feat: add protected route and secure feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Feed from './pages/Feed'
+import ProtectedRoute from './components/ProtectedRoute' // <-- Импортируем нашу таможню
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Feed />} />
+        {/* Публичный роут, сюда пускаем всех */}
         <Route path='/login' element={<Login />} />
+
+        {/* Защищенный роут Ленты */}
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from 'react-router-dom'
+import useAuthStore from '../store/useAuthStore'
+
+export default function ProtectedRoute({ children }) {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated)
+  if (!isAuthenticated) {
+    return <Navigate to='/login' replace={true} />
+  }
+
+  return children
+}
