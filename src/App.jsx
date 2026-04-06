30ec22e1b5bad6de8e9e33c58f6408f35928f375
AlexPlot <plotnikov1988@gmail.com>
2026-04-06 19:24:43 +0200

    feat: setup react router and basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+// Подключаем инструменты роутера
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// Подключаем наши болванки страниц
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+
 function App() {
-  return <div className='text-3xl text-ichgram-blue font-bold'>Тест</div>
+  return (
+    // Оборачиваем всё приложение в провайдер роутера
+    <BrowserRouter>
+      <Routes>
+        {/* Главная страница (Лента) */}
+        <Route path='/' element={<Feed />} />
+        {/* Страница авторизации */}
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div className='text-2xl font-bold p-10'>Страница Логина</div>
+}
