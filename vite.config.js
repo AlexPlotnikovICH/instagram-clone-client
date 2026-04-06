718d07586c58c2009c9d9f2640a666e1b9b8ab05
AlexPlot <plotnikov1988@gmail.com>
2026-04-06 18:03:26 +0200

    chore: setup folder structure and vite proxy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Настройки локального сервера для разработки
+  server: {
+    // Жестко фиксируем порт
+    port: 5173,
+    // Настраиваем прокси для обхода CORS
+    proxy: {
+      // Перехватываем все запросы, начинающиеся с /api
+      '/api': {
+        // Перенаправляем их на твой бэкенд
+        target: 'http://localhost:3333',
+        // Подменяем заголовок origin, чтобы сервер думал, что запрос пришел от него самого
+        changeOrigin: true,
+      },
+    },
+  },
 })
